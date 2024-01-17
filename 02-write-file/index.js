d3df851fd67b9fb886797eee1a3655130da955b5
hakulich <71820310+hakulich@users.noreply.github.com>
2024-01-17 13:28:47 +0100

    feat: add task 2

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,1 +1,22 @@
+const fs = require('fs');
+const path = require('path');
+const { stdout, stdin } = require('process');
 
+const filePath = path.resolve(__dirname, 'text.txt');
+let writeStream = fs.createWriteStream(filePath);
+
+stdout.write('Hello! You can start enter text...\n');
+
+stdin.on('data', (data) => {
+  if (data.toString().trim() === 'exit') {
+    endFunc();
+  }
+  writeStream.write(data);
+});
+
+process.on('SIGINT', endFunc);
+
+function endFunc() {
+  stdout.write('Bye! The entering process has been finished.');
+  process.exit();
+}
